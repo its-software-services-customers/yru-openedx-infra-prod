bb64f80504ab80a03fcd2f51f4c0bd2101b90426
seubpong.mon <pjame.fb@gmail.com>
2021-10-19 21:31:01 +0700

    Added backup VM

--- rke-manager/rke-manager-prod.tf
+++ rke-manager/rke-manager-prod.tf
@@ -41,6 +41,8 @@ locals {
   vcenter_template_or_vm_name = "OPENEDX-RKE-MNGR-TEMPLATE-UBT16-1"
 }
 
+# No longer need VM manager to be created by Terraform
+/*
 module "test-vm-01" {
   source                = "git::https://github.com/its-software-services-devops/tf-module-vsphere-vm.git//modules?ref=1.0.1"
   vm_host               = "yru-prod-rke-manager-01"
@@ -69,5 +71,36 @@ module "test-vm-01" {
   #script_entry_dir = "/tmp"
 
   network_configs       = [{index = 1, use_static_mac = false, mac_address = "", vm_ip = "10.10.5.50", vcenter_network_name = local.vm_network, vm_netmask = 24}]
+  external_disks        = []
+}
+*/
+module "vm-backup-01" {
+  source                = "git::https://github.com/its-software-services-devops/tf-module-vsphere-vm.git//modules?ref=1.0.1"
+  vm_host               = "yru-prod-rke-backup-01"
+  vcenter_vm_name       = "yru-prod-rke-backup-01"
+  vcenter_vapp_name     = ""
+  vm_gateway            = local.gateway
+  vm_guest_id           = local.guest_id
+  vm_dns_list           = local.dns_list
+  num_cpus              = local.num_cpus
+  memory_size           = local.memory_size
+  disk_size             = local.disk_size
+  vcenter_datastore     = local.vm_datastore
+  vcenter_pool_name     = local.vm_poolname
+  vcenter_dc_name       = local.vm_dc_name
+  vcenter_library_name        = local.lib_name
+  vcenter_library_item_name   = local.lib_item_name
+  provisioner_script          = local.test_provisioner
+  vcenter_template_or_vm_name = local.vcenter_template_or_vm_name
+  admin_password        = var.admin_password
+  admin_user            = local.admin_user
+  ssh-pub-key           = var.vm_public_key
+  ssh_ip_address        = "10.10.5.9"
+  disk_thin_provisioned = true
+  wait_for_guest_ip_timeout = 0
+  wait_for_guest_net_timeout = 0
+  #script_entry_dir = "/tmp"
+
+  network_configs       = [{index = 1, use_static_mac = false, mac_address = "", vm_ip = "10.10.5.9", vcenter_network_name = local.vm_network, vm_netmask = 24}]
   external_disks        = []
 }
